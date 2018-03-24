0f156efcdceb8caca9b7078bf358891da9499e67
mvr <matt@example.com>
2018-03-24 01:00:14 -0400

    docker-compose and example data

---
+++ data/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Database configuration
+$dbHost = 'lychee-mysql'; // Host of the database
+$dbUser = 'lychee'; // Username of the database
+$dbPassword = 'lychee'; // Password of the database
+$dbName = 'lychee'; // Database name
+$dbTablePrefix = ''; // Table prefix
+
+?>
